5fd07bf1afc82c4ffdb977bd683694efbffa17df
adarshmalviy <adarshmalvi77@gmail.com>
2024-09-20 14:56:54 +0530

    update: database setup

---
+++ app/db/orm_config.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+from tortoise import Tortoise
+from tortoise.contrib.fastapi import register_tortoise
+
+from app.config import settings
+from app.loggers import logger
+
+
+DATABASE_URL = f"postgres://{settings.database_username}:{settings.database_password}@{settings.database_host}:{settings.database_port}/{settings.database_name}"
+
+logger.debug(f"URL: {DATABASE_URL}")
+
+TORTOISE_ORM = {
+    "connections": {"default": DATABASE_URL},
+    "apps": {
+        "models": {
+            "models": [
+                "app.database.models",
+                "aerich.models",
+            ],
+            "default_connection": "default",
+        }
+    },
+}
+
+
+def init_db(app: FastAPI) -> None:
+    register_tortoise(
+        app=app,
+        config=TORTOISE_ORM,
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+
+
+async def init_tortoise() -> None:
+    await Tortoise.init(config=TORTOISE_ORM)
+
+
+async def generate_schema() -> None:
+    await Tortoise.generate_schemas(safe=True)
+
+
+async def close_db() -> None:
+    await Tortoise.close_connections()
